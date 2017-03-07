30fa7953e2ad050e8780e471b092c396e6902d43
SlevinKelevra <poztalion2@yandex.ru>
2017-03-07 13:11:10 +0300

    Lesson 1

--- Lesson1/Task1.rb
+++ Lesson1/Task1.rb
@@ -4,12 +4,15 @@ name = gets.chomp
 puts "Какой у Вас рост?"
 height = gets.to_i
 
-h = height-110
+if height <= 0
+  puts "Введено некорректное значение роста"
+abort
+end
+
+height = height-110
 
 if height < 0
-  puts "Введено некорректное значение роста"
-elsif h < 0
   puts "Ваш вес уже оптимальный"
 else
-  puts "Привет, #{name}. Твой идеальный все состовляет #{h} кг."
+  puts "Привет, #{name}. Твой идеальный все состовляет #{height} кг."
 end

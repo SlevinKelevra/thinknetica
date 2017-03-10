4c9e1d681771c60d96c1df0b7bc5d9e670fef305
SlevinKelevra <poztalion2@yandex.ru>
2017-03-10 12:50:25 +0300

    Lesson 2

--- Lesson2/Task3.rb
+++ Lesson2/Task3.rb
@@ -1,11 +1,11 @@
-
+f = 0
 f1 = 0
 f2 = 1
 arr = [f1, f2]
- while f1 < 89 do
-  f1 = f1 + f2
-  arr << f1
+ while f < 89 do
+  f = f1 + f2
+  arr << f
    f1 = f2
-   f2 = f1
+   f2 = f
  end
  puts arr
